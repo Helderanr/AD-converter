eaa843e9600f482476dcdfe924267a9433c7cc17
lucassid <lucassidpacheco@gmail.com>
2017-11-01 11:21:01 -0300

    dsuvsbv

--- sine.py
+++ sine.py
@@ -1,26 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.interpolate as inter
-import bitstring
 
-amplitude = 128
-duracao = 10.0
+amplitude = 5.0
+duracao = 100.0
 Fs = 8000
 f = 0.5
-sample = 8000
-sampling = 0.1
+sample = 800 * duracao
+sampling = 3
+b_number = 8
+sens = amplitude / (2 ** b_number - 1)
 
-x = np.arange(0, duracao, duracao/sample)
-y = amplitude * np.sin( np.pi * x * f)
-z = inter.interp1d(x, y, kind='zero', axis=0)
+x = np.arange(0, duracao, duracao / sample)
+y = amplitude * np.sin(np.pi * x * f)
+z = inter.interp1d(x, y, kind = 'zero', axis = 0)
 
-xnew = np.arange(0, duracao, 0.1)
-ynew = z(xnew)   # use interpolation function returned by `interp1d`
-binary = np.empty(ynew.shape)
-#for i in np.nditer(ynew):
+def plotting():
+	global b_number
+	global amplitude
+	global sens
 
-plt.step(x, y, xnew, ynew.astype(int), '-', where="post")
-#plt.plot(x, scipy.signal.cont2discrete(ynew))
-plt.xlabel('Tempo(s)')
-plt.ylabel('Amplitude')
-plt.show()
+	xnew = np.arange(0, duracao, sampling)
+	ynew = z(xnew)
+
+	count = 0
+	ylist = ynew.tolist()
+	for i in ylist:
+		i = int(i / (amplitude / 2 ** b_number))
+		binaryl = '{0:16b}'.format(i)
+		print str(binaryl) + "\n"
+		intyl = int(binaryl, 2)
+		ylist[count] = intyl * sens
+		count = count + 1
+	plt.step(x, y, xnew, ylist, '-', where = "post")
+	plt.xlabel('Tempo(s)')
+	plt.ylabel('Amplitude')
+	plt.show()
+
+if __name__ == "__main__":
+  plotting()
+			
